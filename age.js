6382a1f591cc83854d3aee51034241f3f6f4c700
Sanyyouisf <sanymamdoh@yahoo.com>
2017-01-24 18:50:58 -0600

    donne with the js

--- age.js
+++ age.js
@@ -1,19 +1,6 @@
 //1. How many hours are in a year
-var hoursInYear = 24*7*52
-console.log ("we have " , hoursInYear , " hours in the year")
+var hoursInYear =24
+console.log ("we have "+ hoursInYear +"hours in the year")
 //2. How many minutes are in a decade
-var minInDecade = hoursInYear*60*10
-console.log ("we have " + minInDecade + " minutes in the decade")
 //3. How many seconds old they are
-var year= 35
-var secInAge = hoursInYear*60*60*year
-console.log ("we have " + secInAge + " second in " + year +" years")
 //4. If they are older than some arbitrary number, console "I'm old", else "I'm young"
-if (year >80 ){
-console.log ("I am old");
-} else {
-console.log("I am young");
-}
-
-
-
---
+++ sonnet.js
@@ -0,0 +1,27 @@
+
+var content= document.getElementById("sonnet");
+var sonnet = content.innerHTML;
+// console.log (sonnet);
+
+// Take the contents of the sonnet div and place it in a variable
+var element = document.getElementById('sonnet');
+
+
+// Find and output the starting position of the word "orphans
+var indexOfOrphans = sonnet.indexOf("orphans");
+console.log ("indexOfOrphans" , indexOfOrphans);
+
+// Output the number of characters in the sonnet
+console.log("length of sonnet" , sonnet.length)
+
+
+// Replace the first occurance of the string "winter" with "yuletide"
+var newSonnet = sonnet.replace ("winter" , "yuletide");
+
+
+// Replace all occurances of the string "the" with "a large"
+var globalReplace= newSonnet.replace(/the /g, "a large ");
+
+
+// Set the content of the sonnet div with the new string
+element.innerHTML = globalReplace;
