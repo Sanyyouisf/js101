7391072051f1e0bdef0cdf95d492b0d80e846a73
Sanyyouisf <sanymamdoh@yahoo.com>
2017-01-24 19:22:30 -0600

    finish

---
+++ day2.js
@@ -0,0 +1,23 @@
+switch statment 
+var day = "friday";
+switch (day) {
+case "Monday":
+console.log ("boom");
+break;
+
+case "friday":
+console.log ("party");
+break;
+
+default: 
+console.log ("not monday");
+
+}
+
+if (day === "Monday") {
+console.log("boom");
+} else if (day === "friday") {
+console.log("boom");
+else {
+	console.log("not monday");
+}
