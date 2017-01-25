d8593281900333deebe8a26b0235ed4bfd9a3c94
Sanyyouisf <sanymamdoh@yahoo.com>
2017-01-24 20:49:09 -0600

    done with the arrays

--- day2.js
+++ day2.js
@@ -1,23 +1,131 @@
-switch statment 
-var day = "friday";
-switch (day) {
-case "Monday":
-console.log ("boom");
-break;
+// switch statment 
+// var day = "friday";
+// switch (day) {
+// case "Monday":
+// console.log ("boom");
+// break;
 
-case "friday":
-console.log ("party");
-break;
+// case "friday":
+// console.log ("party");
+// break;
 
-default: 
-console.log ("not monday");
+// default: 
+// console.log ("not monday");
 
-}
+// }
 
-if (day === "Monday") {
-console.log("boom");
-} else if (day === "friday") {
-console.log("boom");
-else {
-	console.log("not monday");
-}
+// if (day === "Monday") {
+// console.log("boom");
+// } else if (day === "friday") {
+// console.log("boom");
+// else {
+// 	console.log("not monday");
+// }
+
+var fruit =["shery","apple","banana"  ]
+var fruitstring = fruit.join ("");
+console.log(fruitstring);
+
+var catcolors ='green,red,yellow'
+var catArray = catcolors.split("yellow");
+console.log(catArray);
+
+var reverseFruit = fruit.reverse();
+console.log(reverseFruit);
+
+var alphFruit = fruit.sort();
+console.log(alphFruit);
+
+var nums = [9,8,7,6,5,4,3];
+var numSort = num.sort(
+	function (a,b){
+		return a-b;
+	}
+
+	);
+console.log(numSort);
+
+var mixCrap =[ "r" ,"m" ,3, 33 ,9]
+console.log (monday);
+console.log(mixCrap.sort());
+
+console.log(fruit.indexOf("banana"))
+
+var animals =['cat','tiger', 'bear','turtle'];
+console.log(animals);
+
+var animals2= ani .slice(2,4);
+console.log(animals2)
+
+animals.splice(1,1);
+console.log(animals)
+
+var colors =["red","blue", "pink","orange"];
+var finalAnswer = colors.sort().reverse().join("cat");//i can put instead of cat ","
+console.log(finalAnswer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
