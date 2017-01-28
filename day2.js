5cf98d030cb8faa1aeef3e76f4e4663fea8ecb3a
Sanyyouisf <sanymamdoh@yahoo.com>
2017-01-28 10:14:03 -0600

    finish

--- day2.js
+++ day2.js
@@ -22,47 +22,67 @@
 // 	console.log("not monday");
 // }
 
-var fruit =["shery","apple","banana"  ]
-var fruitstring = fruit.join ("");
-console.log(fruitstring);
+// var fruit =["shery","apple","banana"  ]
+// var fruitstring = fruit.join ("");
+// console.log(fruitstring);
 
-var catcolors ='green,red,yellow'
-var catArray = catcolors.split("yellow");
-console.log(catArray);
+// var catcolors ='green,red,yellow'
+// var catArray = catcolors.split("yellow");
+// console.log(catArray);
 
-var reverseFruit = fruit.reverse();
-console.log(reverseFruit);
+// var reverseFruit = fruit.reverse();
+// console.log(reverseFruit);
 
-var alphFruit = fruit.sort();
-console.log(alphFruit);
+// var alphFruit = fruit.sort();
+// console.log(alphFruit);
 
-var nums = [9,8,7,6,5,4,3];
-var numSort = num.sort(
-	function (a,b){
-		return a-b;
-	}
+// var nums = [9,8,7,6,5,4,3];
+// var numSort = num.sort(
+// 	function (a,b){
+// 		return a-b;
+// 	}
 
-	);
-console.log(numSort);
+// 	);
+// console.log(numSort);
 
-var mixCrap =[ "r" ,"m" ,3, 33 ,9]
-console.log (monday);
-console.log(mixCrap.sort());
+// var mixCrap =[ "r" ,"m" ,3, 33 ,9]
+// console.log (monday);
+// console.log(mixCrap.sort());
 
-console.log(fruit.indexOf("banana"))
+// console.log(fruit.indexOf("banana"))
 
-var animals =['cat','tiger', 'bear','turtle'];
-console.log(animals);
+// var animals =['cat','tiger', 'bear','turtle'];
+// console.log(animals);
 
-var animals2= ani .slice(2,4);
-console.log(animals2)
+// var animals2= ani .slice(2,4);
+// console.log(animals2)
 
-animals.splice(1,1);
-console.log(animals)
+// animals.splice(1,1);
+// console.log(animals)
 
-var colors =["red","blue", "pink","orange"];
-var finalAnswer = colors.sort().reverse().join("cat");//i can put instead of cat ","
-console.log(finalAnswer);
+// var colors =["red","blue", "pink","orange"];
+// var finalAnswer = colors.sort().reverse().join("cat");//i can put instead of cat ","
+// console.log(finalAnswer);
+
+
+
+
+
+
+// for (var i=0 ; i<5 ;i++) {
+
+// console.log(i);
+// }
+// var fruit= ["cherre", "apple","banana"];
+// for (var j=0 ; j< fruit.length; j++){
+// console.log (fruit[j]);
+// }
+
+for ( var k=0; k<101; k=k+10 ){
+console.log(k);
+}
+
+
 
 
 
